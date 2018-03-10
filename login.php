462719ccd1373d16cf338ccbf1584a78e6669ade
RajivShah8197 <31406000+RajivShah8197@users.noreply.github.com>
2018-03-10 17:34:19 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,21 @@
+</!DOCTYPE html>
+<html>
+  <head>
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="../style/login.css">
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
+  </head>
+
+  <body>
+  <div id="container">
+      <div id="form_box">
+        <form>
+          <p id="form_heading">Login</p>
+          <input type="text" placeholder="Enter your Name" required><br />
+           <input type="password" placeholder="Enter your Password" required><br />
+            <input type="submit" value="Login"><br />
+          </form>
+      </div>
+    </div>
+    </body>
+</html>
---
+++ register.php
@@ -0,0 +1,68 @@
+
+
+  <head>
+    <title>Registration</title>
+    <link rel="stylesheet" type="text/css" href="../style/login.css">
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
+  </head>
+
+  <body>
+    <div id="container">
+      <div id="form_box">
+        <form>
+          <p id="form_heading">Register</p>
+          <input type="text" placeholder="Enter your Name" required><br />
+          <input type="number" placeholder="Age" required min="14" max="99"><br />
+          <input type="password" placeholder="Enter your Password" required><br />
+          <input type="Phone" placeholder="Enter Phone Number" required><br />
+          <!-- Script by hscripts.com -->
+<select name=slist required>
+<option value="Andaman and Nicobar Islands">Andaman and Nicobar Islands</option>
+<option value="Andhra Pradesh">Andhra Pradesh</option>
+<option value="Arunachal Pradesh">Arunachal Pradesh</option>
+<option value="Assam">Assam</option>
+<option value="Bihar">Bihar</option>
+<option value="Chandigarh">Chandigarh</option>
+<option value="Chhattisgarh">Chhattisgarh</option>
+<option value="Dadra and Nagar Haveli">Dadra and Nagar Haveli</option>
+<option value="Daman and Diu">Daman and Diu</option>
+<option value="Delhi">Delhi</option>
+<option value="Goa">Goa</option>
+<option value="Gujarat">Gujarat</option>
+<option value="Haryana">Haryana</option>
+<option value="Himachal Pradesh">Himachal Pradesh</option>
+<option value="Jammu and Kashmir">Jammu and Kashmir</option>
+<option value="Jharkhand">Jharkhand</option>
+<option value="Karnataka">Karnataka</option>
+<option value="Kerala">Kerala</option>
+<option value="Lakshadweep">Lakshadweep</option>
+<option value="Madhya Pradesh">Madhya Pradesh</option>
+<option value="Maharashtra">Maharashtra</option>
+<option value="Manipur">Manipur</option>
+<option value="Meghalaya">Meghalaya</option>
+<option value="Mizoram">Mizoram</option>
+<option value="Nagaland">Nagaland</option>
+<option value="Orissa">Orissa</option>
+<option value="Pondicherry">Pondicherry</option>
+<option value="Punjab">Punjab</option>
+<option value="Rajasthan">Rajasthan</option>
+<option value="Sikkim">Sikkim</option>
+<option value="Tamil Nadu">Tamil Nadu</option>
+<option value="Tripura">Tripura</option>
+<option value="Uttaranchal">Uttaranchal</option>
+<option value="Uttar Pradesh">Uttar Pradesh</option>
+<option value="West Bengal">West Bengal</option>
+</select>
+<!-- Script by hscripts.com -->
+          <input type="text" placeholder="Enter your District/Taluka" required><br />
+          <input type="text" placeholder="Enter your City" required><br />
+           <input type="checkbox" id="NGO"><label for="checkbox" style="color:white; font-size:15px;">NGO</label><br />
+          <input type="checkbox" id="checkbox"><label for="checkbox" style="color:white; font-size:15px;">Remember Me</label><br />
+          <input type="submit" value="Register"><br />
+        </form>
+      </div>
+    </div>
+     
+  </body>
+
+  </html>
